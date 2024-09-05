a8f66b6082899e2b859a4caf763d6cee6db2b604
GrimeLegenda1337 <vanoartemjev@yandex.ru>
2024-09-05 12:02:24 +0300

    Update 4(Без комментариев)

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -59,4 +59,52 @@ RSpec.describe Game do
       expect(chosen_door.get(:open_status)).to be_truthy
     end
   end
+  describe '#player_choose_door_2' do
+    it 'Игрок во второй раз(если предложено и если согласился) выбирает дверь из оставшихся,
+        ту что ещё не выбирал и ту, что не открыта(её выбирал ведущий' do
+      game = Game.new(:classic)
+      player = game.instance_variable_get(:@player)
+      doors = game.instance_variable_get(:@doors)
+      winning_door = game.instance_variable_get(:@winning_door)
+      game.player_choose_door
+      first_choice = player.get(:door_chosen)
+      game.host_choose_door
+      host_choice = doors.find { |door| door.get(:open_status) }
+      game.player_choose_door_2
+      second_choice = player.get(:door_chosen)
+      expect(second_choice).not_to eq(first_choice)
+      expect(second_choice).not_to eq(host_choice)
+    end
+  end
+  describe '#behaviour_variator' do
+    it 'Верно обрабатывает все известные ему варианты' do
+      [:classic,:devil,:angel].each do |behaviour|
+        game = Game.new(behaviour)
+        expect{game.simulate}.to_not raise_error
+      end
+    end
+    it 'Вызывает ошибку для неизвестного поведения' do
+      expect{Game.new(:unknown).simulate}.to raise_error(ArgumentError,"Неизвестный вариант поведения ведущего #{:unknown}")
+    end
+    it 'Собственная функция достоверно сработает для каждого случая поведения' do
+      [:classic, :devil, :angel].each do |behaviour|
+        game = Game.new(behaviour)
+        allow(game).to receive(:offer_change_classic)
+        allow(game).to receive(:offer_change_devil)
+        allow(game).to receive(:offer_change_angel)
+
+        case behaviour
+        when :classic
+          game.simulate
+          expect(game).to have_received(:offer_change_classic).once
+        when :devil
+          game.simulate
+          expect(game).to have_received(:offer_change_devil).once
+        when :angel
+          game.simulate
+          expect(game).to have_received(:offer_change_angel).once
+        end
+      end
+    end
+  end
 end
